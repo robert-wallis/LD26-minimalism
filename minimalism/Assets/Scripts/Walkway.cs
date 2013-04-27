4e1a7efdc1ec226afa282c5a27fdce96eccdafb7
Robert Wallis <robert.wallis@ieee.org>
2013-04-27 16:23:19 -0700

    added powerup art, and objects in the game, but not collectable yet

--- minimalism/Assets/Scripts/Walkway.cs
+++ minimalism/Assets/Scripts/Walkway.cs
@@ -5,20 +5,24 @@ using System.Collections.Generic;
 public class Walkway : MonoBehaviour
 {
 	public GameObject walkwayPrefab;
+	public GameObject powerupPrefab;
 	public GameObject pushObject;
 	public GameObject player;
-	public float walkwayGenerationDistance = 100f;
-	public float walkwayRemovalDistance = 50f;
+	float walkwayGenerationDistance = 100f;
 	Vector3 incrementPosition;
 	Vector3 currentPosition;
 	Quaternion currentRotation;
+	Vector3 powerupHeight = new Vector3(0f, 5f, 0f);
 	LinkedList<GameObject> walkways;
+	LinkedList<GameObject> powerups;
 
 	float lastHigh = 1000f;
+	float generatePowerup = 100f;
 
 	void Start()
 	{
 		walkways = new LinkedList<GameObject>();
+		powerups = new LinkedList<GameObject>();
 		currentPosition = new Vector3(0.0f, -1.0f, 0.0f);
 		currentRotation = walkwayPrefab.transform.rotation;
 		incrementPosition = new Vector3(0.0f, 0.0f, 10.0f);
@@ -35,6 +39,10 @@ public class Walkway : MonoBehaviour
 			GA.API.Design.NewEvent("player:walkway:high", player.transform.position.z);
 			lastHigh = (((int)player.transform.position.z / 1000) * 1000) + 1000f;
 		}
+		if (player.transform.position.z > generatePowerup) {
+			generatePowerup = (((int)player.transform.position.z / 100) * 100) + 100f;
+			GeneratePowerup();
+		}
 	}
 
 	void GenerateWalkways()
@@ -42,17 +50,36 @@ public class Walkway : MonoBehaviour
 		// make new walkways
 		while ((player.transform.position - currentPosition).magnitude < walkwayGenerationDistance) {
 			GameObject newWalkway = (GameObject)Instantiate(walkwayPrefab, currentPosition, currentRotation);
+			newWalkway.name = "walkway " + currentPosition.z;
 			newWalkway.transform.parent = pushObject.transform;
 			walkways.AddLast(newWalkway);
 			currentPosition += incrementPosition;
 		}
-		// this should stop near us
+		// the first walkway ahead of us will stop the loop from deleting valid walkways
 		while (walkways.Count > 0 &&
 				(player.transform.position - walkways.First.Value.transform.position)
-					.magnitude > walkwayRemovalDistance)
+					.z > 20f)
 		{
 			GameObject removable = walkways.First.Value;
 			walkways.RemoveFirst();
+			Destroy(removable);
+		}
+	}
+
+	void GeneratePowerup()
+	{
+		GameObject newPowerup = (GameObject)Instantiate(powerupPrefab, currentPosition + powerupHeight, powerupPrefab.transform.rotation);
+		newPowerup.name = "powerup " + currentPosition.z;
+		newPowerup.transform.parent = pushObject.transform;
+		powerups.AddLast(newPowerup);
+
+		// the first power-up ahead of us will stop the loop from deleting valid power-ups
+		while (powerups.Count > 0 &&
+				(player.transform.position - powerups.First.Value.transform.position)
+					.z > 0f)
+		{
+			GameObject removable = powerups.First.Value;
+			powerups.RemoveFirst();
 			Destroy(removable);
 		}
 	}
